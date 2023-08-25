86b8f6d249922632433e085d3be62957335340ed
bwilkins96 <ben.jr.speed@gmail.com>
2023-08-25 14:13:30 -0400

    login/sign up

---
+++ website/js/template.js
@@ -0,0 +1,44 @@
+// SWDV 640: User Interface Design
+// Concert class and template data rendering
+
+class Concert {
+    constructor(title, venue, time, date, min, max, tickets=0, image='wireframe_img.png', qrImage='qr_code_img.png') {
+        this.title = title;
+        this.venue = venue;
+        this.time = time;
+        this.date = date;
+        this.price = { min: min, max: max };
+        this.tickets = tickets;
+        this.image = 'images/' + image;
+        this.qrImage = 'images/' + qrImage;
+    }
+}
+
+function main() {
+    let concerts = [];
+    
+    if ($('#ticketPage').length) {
+        concerts = [
+            new Concert('Pop Singer', 'Stadium', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00', 2, 'pop.jpg'),
+        ];
+    } else {
+        concerts = [
+            new Concert("Rock N' Roll Tour", 'Outdoor Pavilion', '8:30 PM', 'dd/mm/yyyy', '20.00', '300.00', 0, 'rock.jpg'),
+            new Concert('Pop Singer', 'Stadium', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00', 0, 'pop.jpg'),
+            new Concert('Country Band', 'State Fair', '6:30 PM', 'dd/mm/yyyy', '20.00', '100.00', 0, 'country.jpg'),
+            new Concert('Symphony', 'Local Theatre', '7:00 PM', 'dd/mm/yyyy', '10.00', '50.00', 0, 'classical.jpg')
+        ];
+    }
+    
+
+    const template = $("#concertTemplate").html();
+    Mustache.parse(template);
+
+    const rendered = Mustache.render(template, { concerts: concerts });
+    $("#concertTarget").html(rendered);
+}
+
+$(document).ready(function() {
+    main();
+});
+   
