a245a4448c7aa906c9afa28500f864ecba90877d
bwilkins96 <ben.jr.speed@gmail.com>
2023-08-27 12:37:50 -0400

    simulate cart functionality

---
+++ website/js/cart.js
@@ -0,0 +1,34 @@
+// Simulate 'My Cart' functionality
+
+function clearItems() {
+    const items = document.querySelectorAll('.cartItem');
+    const cartIcons = document.querySelectorAll('.fa-cart-plus');
+
+    for(let item of items) {
+        item.remove();
+    }
+
+    for(let icon of cartIcons) {
+        icon.classList.remove('fa-cart-plus');
+        icon.classList.add('fa-cart-shopping');
+    }
+
+    calcSubTotal(true);
+}
+
+function deleteItem() {
+    clearItems();
+}
+
+function calcSubTotal(cleared=false) {
+    const numInput = document.querySelector('.numTickets');
+    const subTotalDisplay = document.querySelector('.subTotal strong');
+
+    if (cleared) {
+        return subTotalDisplay.innerText = `$0.00`;
+    }
+    
+    const ticketNum = Number(numInput.value);
+    const subTotal = ticketNum * 25;
+    subTotalDisplay.innerText = `$${subTotal}.00`;
+}
